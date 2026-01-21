f4e6e6a14407dd52fa2ed6d22f5f13bea4f837e6
Miguel Vila <miviro0804@gmail.com>
2026-01-21 22:04:12 +0100

    Tracking first approach

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train8/weights/best.pt")
+model = YOLO("runs/detect/seraphim_train/weights/best.pt")
 
-results = model("/home/miviro/Desktop/tfg/yolo/datasets/Anti-UAV-RGBT/val/20190926_195921_1_5/visible.mp4", save=True, show=True)
+#results = model.predict("datasets/drone-tracking/dataset1/cam0.mp4", save=True, show=True)
+results = model.track("datasets/drone-tracking/dataset1/cam0.mp4", save=True, show=True, persist=True)
